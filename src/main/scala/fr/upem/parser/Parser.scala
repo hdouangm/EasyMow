77237158ac6f5ab7843e658920e363bba3fdab1d
Douangmala Hugo <douangmala.hugo@gmail.com>
2019-01-05 19:13:52 +0100

    update and add file fileTest1

--- src/main/scala/fr/upem/parser/Parser.scala
+++ src/main/scala/fr/upem/parser/Parser.scala
@@ -1,8 +1,18 @@
 package fr.upem.parser
 
+import scala.io.Source
+
 /**
   * Created by Hugo on 04/01/2019.
   */
-object Parser {
+object Parser extends App {
+  def readFile(fileName: String): Unit = {
+    for (line <- Source.fromFile(fileName).getLines){
+      print(line);
+    }
+  }
 
+  override def main(args: Array[String]): Unit = {
+    readFile()
+  }
 }
