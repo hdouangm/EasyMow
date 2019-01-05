ec10db7b39769b8dab81dfe93a720ee975d0db99
Douangmala Hugo <douangmala.hugo@gmail.com>
2019-01-05 20:51:14 +0100

    update parser

--- src/main/scala/fr/upem/parser/Parser.scala
+++ src/main/scala/fr/upem/parser/Parser.scala
@@ -1,18 +1,42 @@
 package fr.upem.parser
 
 import scala.io.Source
+import java.nio.file.{Files, Paths}
+
+import fr.upem.coordinate.Coordinate
 
 /**
   * Created by Hugo on 04/01/2019.
   */
 object Parser extends App {
-  def readFile(fileName: String): Unit = {
-    for (line <- Source.fromFile(fileName).getLines){
-      print(line);
+  def readFile(fileName: String): List[String] = {
+    return Source.fromFile(fileName).getLines().toList
+  }
+
+  def readBoardSize(lines: List[String]): Coordinate = {
+    val coordinate = lines(0).split(" ");
+    if (coordinate.length != 2){
+      println("wrong parameters for the board's size");
+      return Coordinate(0, 0);
     }
+    val x = coordinate(0).toInt;
+    val y = coordinate(1).toInt;
+    return Coordinate(x, y)
   }
 
   override def main(args: Array[String]): Unit = {
-    readFile()
+    if (args.length < 1){
+      println("Please, specify the file's name you want to use !");
+    }
+    else {
+      val path = "Files/" + args(0);
+      if (!Files.exists(Paths.get(path))){
+        println("The file you specified does not exist");
+      }
+      else {
+        val lines = readFile(path);
+        val boardCoordinate = readBoardSize(lines);
+      }
+    }
   }
 }
